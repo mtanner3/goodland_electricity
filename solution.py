1302a96e013236f2ac71c830b396eae3bfdd99a8
Matt Tanner <mtanner3@gmail.com>
2016-08-10 17:40:40 -0600

    finally - solution works and is fast enough

--- solution.py
+++ solution.py
@@ -3,27 +3,32 @@ num_cities, tower_range = map(int,raw_input().strip().split())
 tower_range -= 1
 tower_presence_arr = map(int,raw_input().strip().split())
 
-print "tower range = %d" % tower_range
+DEBUG = False
 
 towers_on = 0
 solvable = True
-prev_tpidx = 0
-tpidx = tower_range
+dark_idx = 0
+tpidx = dark_idx + tower_range
 while True:
-    print "prevcity = %d. looking at idx=%d" % (prev_tpidx, tpidx)
+    if DEBUG: print "dark city = %d. looking for tower at idx=%d" % (dark_idx, tpidx),
+    if tpidx >= num_cities: 
+        break
     if tower_presence_arr[tpidx] == 1:
         towers_on += 1
-        print "  Tower. Switching on. %d" % towers_on
+        if DEBUG: print "\n  Switching on tower %d. Total on = %d" % (tpidx, towers_on)
         tpidx += tower_range
-        prev_tpidx = tpidx
-        tpidx += tower_range
-        if tpidx > num_cities:
+        if tpidx >= (num_cities-1):
             break
+        dark_idx = tpidx + 1
+        tpidx = min(dark_idx + tower_range, (num_cities-1))
     else:
-        print "  no tower. Backing up"
+        if DEBUG: print "no tower. Backing up"
         tpidx -= 1
-        if tpidx == prev_tpidx:
+        if tpidx < (dark_idx - tower_range):
             solvable = False
             break
 
-print towers_on
+if solvable is False:
+    print "-1"
+else:
+    print towers_on
